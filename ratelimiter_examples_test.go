eae935a1dfbac22772b1ce63f058ab9e69225b7e
RussellLuo <luopeng.he@gmail.com>
2017-06-10 10:12:25 +0800

    Update the testable examples

--- ratelimiter_examples_test.go
+++ ratelimiter_examples_test.go
@@ -2,7 +2,6 @@ package ratelimiter_test
 
 import (
 	"fmt"
-	"sync"
 	"time"
 
 	"github.com/RussellLuo/ratelimiter"
@@ -17,58 +16,23 @@ func (r *Redis) Eval(script string, keys []string, args ...interface{}) (interfa
 	return r.client.Eval(script, keys, args...).Result()
 }
 
-func ExampleTake() {
-	limiter := ratelimiter.New(
+func Example() {
+	rl := ratelimiter.New(
 		&Redis{redis.NewClient(&redis.Options{
 			Addr: "localhost:6379",
 		})},
-		"ratelimiter:test1",
+		"ratelimiter:example",
 		&ratelimiter.Bucket{
 			Interval: 1 * time.Second,
-			Quantum:  5,
+			Quantum:  2,
 			Capacity: 10,
 		},
 	)
-	if ok, _ := limiter.Take(1); ok {
+	if ok, _ := rl.Take(1); ok {
 		fmt.Println("PASS")
 	} else {
 		fmt.Println("DROP")
 	}
 	// Output:
-	// PASS
-}
-
-func ExampleTake_concurrency() {
-	concurrency := 5
-	limiter := ratelimiter.New(
-		&Redis{redis.NewClient(&redis.Options{
-			Addr: "localhost:6379",
-		})},
-		"ratelimiter:test2",
-		&ratelimiter.Bucket{
-			Interval: 1 * time.Second,
-			Quantum:  5,
-			Capacity: 10,
-		},
-	)
-
-	var wg sync.WaitGroup
-	for i := 0; i < concurrency; i++ {
-		wg.Add(1)
-		go func() {
-			if ok, _ := limiter.Take(1); ok {
-				fmt.Println("PASS")
-			} else {
-				fmt.Println("DROP")
-			}
-			wg.Done()
-		}()
-	}
-	wg.Wait()
-	// Output:
-	// PASS
-	// PASS
-	// PASS
-	// PASS
 	// PASS
 }
